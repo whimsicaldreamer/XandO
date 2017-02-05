0735d7cacd9c9726332cec67179ad8f693d00905
Ayan Dey <ayand04@gmail.com>
2017-02-05 17:54:28 +0530

    Update meta tag

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
     <title>noughts N crosses</title>
     <link href="images/ico.png" type="image/x-icon" rel="shortcut icon" />
     <link href="css/bootstrap.min.css" rel="stylesheet">
