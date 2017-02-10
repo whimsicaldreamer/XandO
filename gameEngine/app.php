ba954e96f0bc6b40f92372eae4555dc014e401a6
Ayan Dey <ayand04@gmail.com>
2017-02-10 13:08:53 +0530

    Update page extension

--- gameEngine/app.php
+++ gameEngine/app.php
@@ -21,5 +21,5 @@ if(isset($_POST['playerName']) && isset($_POST['gridSize'])) {
     $gameRoom = $gameHandler->createRoom(); // Generate a room
     $gameHandler->setPlayer($playerName, $gridSize, $gameRoom); // Set the player with a room
 
-    echo "playground.html?r=".$gameRoom."&s=".$gridSize; // Generate new room link
+    echo "playground.php?r=".$gameRoom."&s=".$gridSize; // Generate new room link
 }
