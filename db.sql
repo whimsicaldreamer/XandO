b670a23b086ff46f29be79d7ce41f6bab08ae612
Ayan Dey <ayand04@gmail.com>
2017-03-24 13:26:45 +0530

    Add sessionId column to existing structure

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2017 at 07:42 AM
+-- Generation Time: Mar 24, 2017 at 07:55 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
